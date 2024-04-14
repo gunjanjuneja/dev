9f7acdc95119b47a9998c05b75ec221d2dc0bf84
gunjanjuneja <90602002+gunjanjuneja@users.noreply.github.com>
2024-04-14 14:47:04 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['dev-00em.onrender.com','*']
 
 
-#CSRF_TRUSTED_ORIGINS = ['https://www.edenthought.com']
+CSRF_TRUSTED_ORIGINS = ['https://dev-00em.onrender.com']
 
 
 # Set allowed cidr nets
